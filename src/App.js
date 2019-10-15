582c2cfc5ca606f1ab614462391e75ad6ba286b0
Kevin at MMS <x8neter@gmail.com>
2019-10-15 17:29:01 +1300

    at Office

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import BookContextProvider from "./contexts/BookContext";
 import Navbar from "./components/Navbar";
+import BookList from "./components/BookList";
+import NewBookForm from "./components/NewBookForm";
 
 function App() {
   return (
     <div className="App">
       <BookContextProvider>
         <Navbar />
+        <BookList />
+        <NewBookForm />
       </BookContextProvider>
     </div>
   );
--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -3,6 +3,12 @@ import { BookContext } from "../contexts/BookContext";
 
 const BookDetails = ({ book }) => {
   const { removeBook } = useContext(BookContext);
+  return (
+    <li onClick={() => removeBook(book.id)}>
+      <div className="title">{book.title}</div>
+      <div className="author">{book.author}</div>
+    </li>
+  );
 };
 
 export default BookDetails;
